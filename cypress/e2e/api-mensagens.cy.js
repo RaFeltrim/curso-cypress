c5a2f3406e795741c4ba23d4c97042ec9f219de9
Rafael Feltrim <rafeltrim@gmail.com>
2025-05-02 16:03:10 -0300

    last update, corrigindo erros do código

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -4,13 +4,11 @@ describe('Api Adopet', () => {
     // const tempoEsperado = Math.random() * 1000        //flakytest
     // expect(res.duration).to.be.lte(tempoEsperado)     //flakytest      
 
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlNDMyNWJhNS05ZmEzLTQwZTYtYmY3Ni0xMmE4MDJlN2JhMTUiLCJhZG9wdGVyTmFtZSI6IlJhZmFlbCIsImlhdCI6MTc0NjEyNzkxNSwiZXhwIjoxNzQ2Mzg3MTE1fQ.pyKg3s5l1HlNe5BZOHInEAuephS6BimvYkLfQspAnF0`
-
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET' ,
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/e4325ba5-9fa3-40e6-bf76-12a802e7ba15',
-            headers: { authorization }           
+            headers: Cypress.env()         
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
